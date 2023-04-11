576f57e50875a5516c5e155de1d8282b0c02036a
Arnab-1473 <arban.nilr.1473@gmail.com>
2023-04-12 00:05:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
 //complete this code
 class Person {
-    constructor(name, age){
+    constructor(name, age) {
         this._name = name;
         this._age = age;
-    }
-    get name(){
+      }
+      
+      get name() {
         return this._name;
-    }
-    set age(age){
+      }
+      
+      set age(age) {
         this._age = age;
-    }
+      }
+      
+      get age() {
+        return this._age;
+      }
 }
 
 class Student extends Person {
-    study(){
-        console.log(`${this._name} is studying);
-    }
+    study() {
+        console.log(`${this.name} is studying.`);
+      }
 }
 
 class Teacher extends Person {
-    teach(){
-        console.log(`${this._name} is teaching`);
-    }
+    teach() {
+        console.log(`${this.name} is teaching.`);
+      }
 }
 
 // Do not change the code below this line
